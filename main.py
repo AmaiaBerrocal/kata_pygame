6a644657b0f0af23887170f2615f97c3c6caaf10
AmaiaBerrocal <amaia.berrocal@gmail.com>
2020-01-21 19:31:53 +0100

    Animando al robot

--- main.py
+++ main.py
@@ -8,40 +8,65 @@ init()
 
 class Robot:
     speed = 5
-    images = ['robot_r01.png', 'robot_r02.png', 'robot_r03.png', 'robot_r04.png']
+    pictures = ['robot_r01.png', 'robot_r02.png', 'robot_r03.png', 'robot_r04.png']
 
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
-        self.image = image.load('resources/robot_r01.png').convert_alpha()
+        
+        self.frames = []
+        for pict in self.pictures:
+            frame = image.load('resources/{}'.format(pict)).convert_alpha()
+            self.frames.append(frame)
+        #self.frames = [image.load('resources/{}'.format(pict)).convert_alpha() for pict in self.pictures]   es el mismo for que arriba
+        
+        self.frame_act = 0
+        self.num_frames = len(self.frames)
+
+    def change_frame(self):
+        self.frame_act += 1
+        if self.frame_act == self.num_frames:
+            self.frame_act = 0
+        #self.frame_act = (self.frame_act + 1) % self.num_frames  equivale a las 3 lineas de arriba. si frame_act=0, le voy a sumar 1. 1/4 el resto es 1. 2/4 resto 2, 3/4 resto 3, 4/4 resto 0: vuelve a posicion cero
 
     def go_up(self):
         #if self.y > 0:
         #   self.y -= self.speed
         self.y = max(0, self.y - self.speed) #self.y es el maximo entre self.y menos speed. Si self.y=100 va a ser el max entre 0 y 95, asi que sera 95. Si self.y=0 el max entre 0 y -5 es 0.
+        self.change_frame()
 
     def go_down(self):
         self.y = min(600, self.y + self.speed)
+        self.change_frame()
 
     def go_left(self):
         self.x = max(0, self.x - self.speed)
+        self.change_frame()
 
     def go_right(self):
         self.x = min(800, self.x + self.speed)
+        self.change_frame()
 
     @property
     def position(self):
         return self.x, self.y
 
+    @property
+    def image(self):
+        return self.frames[self.frame_act]
+
 #Creamos la pantalla
 screen = display.set_mode((800, 600))
 display.set_caption('Título de la pantalla')
+clock = time.Clock()
+
 background_color = (150, 150, 222)
 
 robot = Robot(400, 300)
 
 #Bucle básico
 while True:
+    dt = clock.tick(60)
     for ev in event.get():
         if ev.type == QUIT:
             quit()
