4a2a3642be4f9ad14d45fd52bc8ac8e3e682b540
AmaiaBerrocal <amaia.berrocal@gmail.com>
2020-01-26 12:43:07 +0100

    Refactorización y separación de lógica y objetos

--- main.py
+++ main.py
@@ -3,101 +3,10 @@ from pygame import *   #podemos usar la clase de pygame sin poner nada delante
 from pygame.locals import * #contiene varias constantes como las teclas
 import sys
 from random import randint
+from entities import *
 
 #inicializamos pygame
 init()
-FPS = 60
-
-class Bomb(sprite.Sprite):
-    pictures = ['bomb_01.png', 'bomb_02.png', 'bomb_03.png', 'bomb_04.png']
-    w = 44
-    h = 42
-
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-        sprite.Sprite.__init__(self)
-        self.rect = Rect(self.x, self.y, self.w, self.h)
-        
-        self.frames = []
-        for pict in self.pictures:
-            frame = image.load('resources/{}'.format(pict)).convert_alpha()
-            self.frames.append(frame)
-            
-        self.frame_act = 0
-        self.num_frames = len(self.frames)
-        
-        self.current_time = 0
-        self.animation_time = FPS//60
-
-    def update(self, dt):
-        self.current_time += dt
-        if self.current_time >= self.animation_time:
-            self.current_time = 0
-            self.frame_act += 1
-            if self.frame_act == self.num_frames:
-                self.frame_act = 0
-    
-    @property
-    def image(self):
-        return self.frames[self.frame_act]
-
-
-class Robot(sprite.Sprite):
-    speed = 5
-    pictures = ['robot_r01.png', 'robot_r02.png', 'robot_r03.png', 'robot_r04.png']
-    w = 64
-    h = 68
-    lives = 3
-
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-        sprite.Sprite.__init__(self)
-        self.rect = Rect(self.x, self.y, self.w, self.h)
-         
-        self.frames = []
-        for pict in self.pictures:
-            frame = image.load('resources/{}'.format(pict)).convert_alpha()
-            self.frames.append(frame)
-        #self.frames = [image.load('resources/{}'.format(pict)).convert_alpha() for pict in self.pictures]   es el mismo for que arriba
-        
-        self.frame_act = 0
-        self.num_frames = len(self.frames)
-
-    def change_frame(self):
-        self.frame_act += 1
-        if self.frame_act == self.num_frames:
-            self.frame_act = 0
-        #self.frame_act = (self.frame_act + 1) % self.num_frames  equivale a las 3 lineas de arriba. si frame_act=0, le voy a sumar 1. 1/4 el resto es 1. 2/4 resto 2, 3/4 resto 3, 4/4 resto 0: vuelve a posicion cero
-
-    def go_up(self):
-        self.rect.y = max(0, self.rect.y - self.speed) #self.y es el maximo entre self.y menos speed. Si self.y=100 va a ser el max entre 0 y 95, asi que sera 95. Si self.y=0 el max entre 0 y -5 es 0.
-        self.change_frame()
-
-    def go_down(self):
-        self.rect.y = min(600, self.rect.y + self.speed)
-        self.change_frame()
-
-    def go_left(self):
-        self.rect.x = max(0, self.rect.x - self.speed)
-        self.change_frame()
-
-    def go_right(self):
-        self.rect.x = min(800, self.rect.x + self.speed)
-        self.change_frame()
-
-    def comprobarToques(self, group):
-        colisiones = sprite.spritecollide(self, group, True)
-        for b in colisiones:
-            self.lives -= 1
-        return self.lives #desde aqui no podemos llamar a gameOver porque esta en Game, por eso devolvemos el numero de vidas
-
-    @property
-    def image(self):
-        return self.frames[self.frame_act]
 
 
 class Game:
@@ -166,6 +75,7 @@ class Game:
             self.all_group.draw(self.screen)
 
             display.flip() #refresca o redibuja
+
 
 if __name__ == '__main__':
     game = Game()
